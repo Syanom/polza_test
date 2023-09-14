505e9d7ef66ed16602f6556002bd98d1f9a18793
Syanom <yandupliy@gmail.com>
2023-09-14 23:55:20 +0300

    refactor kitchen order creator

--- app/models/menu_order.rb
+++ app/models/menu_order.rb
@@ -2,4 +2,12 @@ class MenuOrder < ApplicationRecord
   belongs_to :menu
   has_many :ingredient_to_ignores
   has_many :excluded_ingredients, through: :ingredient_to_ignores, source: :ingredient
+
+  def dishes
+    excluded_ingredient_ids = excluded_ingredients.map(&:id)
+    dish_ids = menu.dishes.map do |dish|
+      dish.id unless dish.ingredients.where(id: excluded_ingredient_ids).present?
+    end
+    Dish.where(id: dish_ids)
+  end
 end
--- app/services/kitchen_order_creator.rb
+++ app/services/kitchen_order_creator.rb
@@ -6,13 +6,11 @@ class KitchenOrderCreator
   def create
     dishes_with_count = {}
     @menu_orders.each do |order|
-      order.menu.dishes.each do |dish|
-        unless dish.ingredients.where(id: order.excluded_ingredients.map(&:id)).present?
-          if dishes_with_count[dish.name.to_sym].present?
-            dishes_with_count[dish.name.to_sym] += 1
-          else
-            dishes_with_count[dish.name.to_sym] = 1
-          end
+      order.dishes.each do |dish|
+        if dishes_with_count[dish.name.to_sym]
+          dishes_with_count[dish.name.to_sym] += 1
+        else
+          dishes_with_count[dish.name.to_sym] = 1
         end
       end
     end
