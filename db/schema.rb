e7a83b9aae9274201a08887bcec6614ab06c33f4
Syanom <yandupliy@gmail.com>
2023-09-14 11:49:32 +0300

    create dishe model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_14_084634) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_084806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "menus", force: :cascade do |t|
     t.datetime "created_at", null: false
