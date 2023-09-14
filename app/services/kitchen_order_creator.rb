4e5706cb8d757a81ab15e0cb896222fa0512b59c
Syanom <yandupliy@gmail.com>
2023-09-15 00:21:32 +0300

    refactor ketchen order creator even more

--- app/services/kitchen_order_creator.rb
+++ app/services/kitchen_order_creator.rb
@@ -4,18 +4,9 @@ class KitchenOrderCreator
   end
 
   def create
-    dishes_with_count = {}
-    @menu_orders.each do |order|
-      order.dishes.each do |dish|
-        if dishes_with_count[dish.name.to_sym]
-          dishes_with_count[dish.name.to_sym] += 1
-        else
-          dishes_with_count[dish.name.to_sym] = 1
-        end
-      end
-    end
-
-    dishes_with_count_array = dishes_with_count.map { |dish, count| { name: dish, count: } }
-    dishes_with_count_array.sort_by { |dish| -dish[:count] }
+    dishes_with_count = @menu_orders.map { |order| order.dishes.to_a }.flatten.tally
+    @menu_orders.update_all(delivered_to_kitchen: true)
+    dishes_with_count.map { |dish, count| { name: dish.name, count: } }
+                     .sort_by { |dish| -dish[:count] }
   end
 end
