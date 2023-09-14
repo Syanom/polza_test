b3b94e5460ba103d2bb38410dd6c950d4a537667
Syanom <yandupliy@gmail.com>
2023-09-14 12:29:44 +0300

    fix dish-menu association

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,4 @@
 class Dish < ApplicationRecord
+  has_many :dish_in_menus
   has_many :menus, through: :dish_in_menus
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,5 @@
 class Menu < ApplicationRecord
+  has_many :dish_in_menus
   has_many :dishes, through: :dish_in_menus
   has_many :menu_orders
 end
