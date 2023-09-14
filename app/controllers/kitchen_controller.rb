bead54acb62fb24f913dd036c2c8271371560305
Syanom <yandupliy@gmail.com>
2023-09-14 21:02:48 +0300

    remove debug code

--- app/controllers/kitchen_controller.rb
+++ app/controllers/kitchen_controller.rb
@@ -1,6 +1,5 @@
 class KitchenController < ApplicationController
   def orders_to_cook
-    orders = [{ name: 'Омлет с брокколи', count: 10 }, { name: 'Овсяный сырник с вишней', count: 9 }]
     render json: KitchenOrderCreator.new.create
   end
 end
